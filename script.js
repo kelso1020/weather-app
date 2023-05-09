7d1ad56921803197f2139099cc327845fbda1f7b
kelso1020 <kelseyjessica1710@gmail.com>
2023-05-09 15:27:56 -0400

    adjusting styles to give webpage better appearance

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const searchHistory = document.getElementById('search-history');
 const currentWeather = document.getElementById('current-weather');
 const fiveDayForecast = document.getElementById('forecast');
 
+displaySearchHistory();
+
 // retrieves current weather data for searched city
 function getWeather(city) {
     baseURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
@@ -23,18 +25,20 @@ function getForecast(city) {
     });
 };
 
+// function to retreive data for the current weather in the most recently searched/selected city
 function createArticle(currentWeather) {
+    // creating elements to fill currentWeather section in html
     var articleEl = document.createElement('article');
     var h2El = document.createElement('h2');
     var pEl = document.createElement('p');
-
+    // setting heading to searched city name and date
     h2El.textContent = currentWeather.name + ' (' + new Date(currentWeather.dt * 1000).toLocaleDateString() + ')';
-
+    // variables to identify and display weather icon
     var weatherIcon = currentWeather.weather[0].icon;
     var iconURL = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
     var iconEl = document.createElement('img');
     iconEl.src = iconURL;
-    
+    // setting retreived data to show up as a string of text in the injected paragraph element
     pEl.innerHTML = "Temperature: " + currentWeather.main.temp + "°F<br>"
     + "Wind: " + currentWeather.wind.speed + "MPH<br>"
     + "Humidity: " + currentWeather.main.humidity + "%";
@@ -44,11 +48,13 @@ function createArticle(currentWeather) {
     return articleEl;
 };
 
+// function to retrieve data for five days of the weather forecast for searched city
 function createForecastCard(fiveDayForecast) {
+    // creating elements to fill the forecast section in html
     var divEl = document.createElement('div');
     var h2El = document.createElement('h2');
     var pEl = document.createElement('p');
-
+    // setting heading of each forecast card to correlating date and weather 
     h2El.textContent = new Date(fiveDayForecast.dt * 1000).toLocaleDateString() + ')';
 
     var forecastIcon = fiveDayForecast.weather[0].icon;
@@ -65,6 +71,7 @@ function createForecastCard(fiveDayForecast) {
     return divEl;
 };
 
+// displays retreived data from the previous 2 functions to the correct sections in html
 function displayWeather(city) {
     getWeather(city)
     .then (function(data) {
@@ -75,10 +82,11 @@ function displayWeather(city) {
 
     getForecast(city) 
         .then(function(data) {
+            // pulling data from 12 noon each day to represent daily weather in five day forecast
             var dailyWeather = data.list.filter(function(item) {
                 return item.dt_txt.includes('12:00:00');
             });
-
+            // creates individual card for each of the five days of the forecast so data displays properly
             fiveDayForecast.innerHTML = "";
             for (var forecast of dailyWeather) {
                 var forecastCard = createForecastCard(forecast);
@@ -87,10 +95,12 @@ function displayWeather(city) {
         });
 };
 
+
 function displaySearchHistory() {
+    // retreives data from local storage to display on webpage
     var searchedList = JSON.parse(localStorage.getItem('searchHistory')) || [];
     searchHistory.innerHTML = "";
-
+    // assigns each of the city names saved in local storage to its own li element to display as list under input form
     for (var city of searchedList) {
         var liEl = document.createElement('li');
         liEl.textContent = city;
@@ -99,26 +109,29 @@ function displaySearchHistory() {
     };
 };
 
+// event listener submit allows use of 'enter' key to submit form
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
+    // the value typed into the search input
     var cityName = city.value;
-
+    // each value entered and searched is saved to local storage
     let searchedList = JSON.parse(localStorage.getItem('searchHistory')) || [];
-
+    // checking if a city name is already saved in local storage, and if there is push to display 
     if (searchedList.indexOf(cityName) === -1) {
         searchedList.push(cityName);
         localStorage.setItem('searchHistory', JSON.stringify(searchedList));
         displaySearchHistory();
     };
-
+    // when a city name is typed in input and searched that city name runs through the display weather function
     city.value = "";
     displayWeather(cityName);
 });
-
+// when a list item in the search history is clicked, the webpage will load to that city's weather data
 searchHistory.addEventListener('click', function(event) {
     if (event.target.matches('.search-history-item')) {
         var searchedCity = event.target.textContent;
         displayWeather(searchedCity);
     };
 });
+
+
